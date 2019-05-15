8c803b93c15b02976ff789fac8574329ed5b3e5b
Robert <robert.mendoza8@gmail.com>
2019-05-15 15:36:16 -0400

    Sprint 9 Get Tags Artifact

--- src/test/java/com/riskiq/api/v2/stepdefinitions/tagArtifact/impl/TagArtifact.java
+++ src/test/java/com/riskiq/api/v2/stepdefinitions/tagArtifact/impl/TagArtifact.java
@@ -20,49 +20,10 @@ public class TagArtifact {
         response.set(rs.get().contentType(ContentType.TEXT).body(dataTableToJson(dataTable.asList(BodyElement.class))).get(setMethodAndEndPoint(GET,Api)));
     }
 
-    public static void PostWithDataTable(String Api, DataTable dataTable){
-        response.set(rs.get().contentType(ContentType.TEXT).body(dataTableToJson(dataTable.asList(BodyElement.class))).post(setMethodAndEndPoint(POST,Api)));
-        setArtifact(Artifact.with()
-          .query(response.get().path("query"))
-          .tagsArtifact(response.get().path("tags"))
-          .create());
-    }
-
-    public static void PutSetTagsWithDataTable(String Api, DataTable dataTable){
-        response.set(rs.get().contentType(ContentType.TEXT).body(dataTableToJson(dataTable.asList(BodyElement.class))).put(setMethodAndEndPoint(PUT,Api)));
-    }
-
-    public static void PostClassificationWithDataTable(String Api, DataTable dataTable){
-        response.set(rs.get().contentType(ContentType.TEXT).body(dataTableToJson(dataTable.asList(BodyElement.class))).post(setMethodAndEndPoint(POST,Api)));
-    }
-
-    public static void DeleteWithDataTable(String Api, DataTable dataTable){
-        response.set(rs.get().contentType(ContentType.TEXT).body(dataTableToJson(dataTable.asList(BodyElement.class))).delete(setMethodAndEndPoint(DELETE,Api)));
-    }
-
-    public static void setBulkClassification(int cant, String bodyJsonArtifact){
-       response.set(rs.get().contentType(ContentType.TEXT).body(bodyJsonArtifact).post(setMethodAndEndPoint(POST,actionsBulkClassification)));
-    }
-
-    public static void getBulkClassification(int cant, String bodyJsonArtifact){
-        response.set(rs.get().contentType(ContentType.TEXT).body(bodyJsonArtifact).get(setMethodAndEndPoint(GET,actionsBulkClassification)));
-    }
 
     //Class Methods
 
-    public static void AddTags(DataTable dataTable){
-        PostWithDataTable(actionsTags, dataTable);
-    }
-
-    public static void SetTags(DataTable dataTable){
-        PutSetTagsWithDataTable(actionsTags, dataTable);
-    }
-
-    public static void DeleteTags(DataTable dataTable){
-        DeleteWithDataTable(actionsTags, dataTable);
-    }
-
-    public static void GetTags(DataTable dataTable){
+   public static void GetTags(DataTable dataTable){
         GetWithDataTable(artifactTag, dataTable);
     }
 
